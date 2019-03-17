20f82144aba96a9511f9dbc24c3913ea9bfd0c40
FAmura <federicoamura@gmail.com>
2019-03-17 02:36:26 -0300

    Agregado boton de retorno en la vista de resultados de busqueda

--- app/src/main/java/tdp2/tp0app/ResultsActivity.java
+++ app/src/main/java/tdp2/tp0app/ResultsActivity.java
@@ -3,6 +3,8 @@ package tdp2.tp0app;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -12,11 +14,27 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle(getIntent().getStringExtra("QUERY"));
 
         ArrayList<Book> books = (ArrayList<Book>) getIntent().getSerializableExtra("RESPONSE");
 
         RecyclerView booksList = findViewById(R.id.booksList);
         booksList.setAdapter(new BooksAdapter(ResultsActivity.this, books));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
